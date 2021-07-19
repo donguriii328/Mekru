fe53f2ac8fc06872b7cf53f66ad261ee9b14b2b0
DESKTOP-9A9UVMB\i022ff <i022ff@yamaguchi-u.ac.jp>
2021-07-19 19:58:48 +0900

    4th Commit

--- app/src/main/java/com/example/tyokoku/StampActivity.java
+++ app/src/main/java/com/example/tyokoku/StampActivity.java
@@ -6,10 +6,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -20,9 +24,19 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StampActivity extends AppCompatActivity {
+    static List<String> items = new ArrayList<String>();
+    static ArrayAdapter<String> adapter;
+    static MyDbHelper dbAdapter;
+
+
+    ListView listView1;
+    ListView myListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,43 +46,31 @@ public class StampActivity extends AppCompatActivity {
         Button returnButton = findViewById(R.id.returnbutton);
         returnButton.setOnClickListener(v -> finish());
 
-        /*
+
+        listView1 = (ListView) findViewById(R.id.listview1);
+
         //db
         MyDbHelper mDbHelper = new MyDbHelper(this);
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
         //select
-        Cursor c = db.rawQuery("select * from members",null);
+        Cursor c = db.rawQuery("select * from sculputure", null);
 
         //adapterの準備
         //表示するカラム名
-        String[] from = {"name","_id"};
+        String[] from = {"_id", "name"};
         //バインドするViewリソース
-        int[] to = {android.R.id.text1,android.R.id.text2};
+        //int[] to = {android.R.id.text1, android.R.id.text2};
 
         //adapter生成
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,c,from,to,0);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, from);
 
-        //bindして表示
-        myListView.setAdapter(adapter);
-        //クリックしたとき各行のデータ（特に_id）を取得
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                //各要素を取得
-                String s1 = ((TextView)view.findViewById(android.R.id.text1)).getText().toString();
-                String s2 = ((TextView)view.findViewById(android.R.id.text2)).getText().toString();
-
-                Log.v("tama","position=" + s1);
-                Log.v("tama","position=" + s2);
-            }
-        });
-
-        //loop(いらない）
-        while(c.moveToNext()){
-            Log.v("tama", c.getString(c.getColumnIndex("name")));
-        }*/
+        listView1.setAdapter(adapter);
 
     }
 }
+
+
+
+
